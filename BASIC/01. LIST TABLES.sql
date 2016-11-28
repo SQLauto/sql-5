8caff4066ec3574dd39a48263b979cafeaa1ac51
Richard <richardrulach@users.noreply.github.com>
2016-11-28 10:35:49 +0000

    Update 01. LIST TABLES.sql

--- BASIC/01. LIST TABLES.sql
+++ BASIC/01. LIST TABLES.sql
@@ -1,3 +1,15 @@
+/**
+Author:		RR
+Created:	28/11/2016
+Description:	Uses information schema views to get a list of tables and the 
+		schema they are in.
+
+UPDATES
+---------------------------------------------------------------------------------
+DATE		EDITOR	DESCRIPTION
+28/11/2016	RR	File created
+
+**/
 SELECT 
 	TABLE_SCHEMA,
 	TABLE_NAME
