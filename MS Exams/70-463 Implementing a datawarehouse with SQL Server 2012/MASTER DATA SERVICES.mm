21d4d01732e77a56056274d46032f86e4b8707d1
Richard <richardrulach@gmail.com>
2017-08-31 06:39:30 +0100

    New mind maps

---
+++ MS Exams/70-463 Implementing a datawarehouse with SQL Server 2012/MASTER DATA SERVICES.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1504157093124" ID="ID_617989885" MODIFIED="1504157103174" TEXT="MASTER DATA SERVICES">
+<node CREATED="1504157111786" ID="ID_1890603745" MODIFIED="1504157123271" POSITION="right" TEXT="PRACTICAL"/>
+<node CREATED="1504157105018" ID="ID_275606451" MODIFIED="1504157108463" POSITION="left" TEXT="THEORY"/>
+</node>
+</map>
